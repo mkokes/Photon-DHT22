53c9295c863e80ecaaaf9cc978fd2548edac5fd1
Marty Kokes <mkokes@gmail.com>
2015-12-29 01:26:41 -0600

    Little bit of cleaning up, added all of the different reading options in and commented the ones I'm not personally using out.

--- DHT22.ino
+++ DHT22.ino
@@ -3,7 +3,7 @@
 
 // DHT parameters
 #define DHTTYPE  DHT22              // Sensor type DHT11/21/22/AM2301/AM2302
-#define DHTPIN   D1         	    // Digital pin for communications
+#define DHTPIN   D1         	      // Digital pin for communications
 #define DHT_SAMPLE_INTERVAL   2000  // Sample every two seconds
 
 // Variables
@@ -11,21 +11,20 @@ int temperature;
 int humidity;
 int blue = D7;
 
-//declaration
-void dht_wrapper(); // must be declared before the lib initialization
+// Declaration
+void dht_wrapper();
 
 // Lib Initialize
 PietteTech_DHT DHT(DHTPIN, DHTTYPE, dht_wrapper);
 
 // globals
-unsigned int DHTnextSampleTime;	    // Next time we want to start sample
-bool bDHTstarted;		    // flag to indicate we started acquisition
-int n;                              // counter
+unsigned int DHTnextSampleTime; // Next time we want to start sample
+bool bDHTstarted;		            // flag to indicate we started acquisition
+int n;                          // counter
 
 void setup() {
     pinMode(blue, OUTPUT);
     DHTnextSampleTime = 0;
-
 }
 
 void dht_wrapper() {
@@ -52,11 +51,19 @@ void loop()
     int dp = DHT.getDewPoint();
     int dpslow = DHT.getDewPointSlow();
 
+    //Turn the built in LED on to indicate publishing
     digitalWrite(blue, HIGH);
+
+    //* Publish readings - uncomment/comment the values you'd like to publish
+
     Particle.publish("Humidity", String(humidity) + "%");
     Particle.publish("Temperature", String(tempf) + " °F");
+    //Particle.publish("Temperature", String(tempc) + " °C");
+    //Particle.publish("Temperature", String(tempk) + " °K");
+    //Particle.publish("Dew Point", String(dp) + " %");
     //Particle.publish("Dew Point", String(dpslow) + " %");
 
+    //Turn the built in LED off indicate publishing finished
     delay(250);
     digitalWrite(blue, LOW);
 
